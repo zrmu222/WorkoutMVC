aa4ffdabc5a34de52c388f37db3812a9a2b8aec6
zrmu222 <zmurphy@regis.edu>
2017-04-20 22:18:52 -0600

    Added Server Test

---
+++ WorkoutMVC.Tests/Model/Server/ServerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkoutMVC.Tests.Model.Server
+{
+    [TestClass]
+    public class ServerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
