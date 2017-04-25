34389ea85a3817fba407f9756b47898931423ebd
Zack Murphy <zrm725@yahoo.com>
2017-04-25 11:40:22 -0600

    Update

---
+++ WorkoutMVC.Tests/Model/WCF/WCFTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkoutMVC.Models.WCF;
+using LibMyWorkout.Domain;
+
+namespace WorkoutMVC.Tests.Model.WCF
+{
+    [TestClass]
+    public class WCFTest
+    {
+        WCFClient proxy;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            proxy = new WCFClient();
+        }
+
+
+        [TestMethod]
+        public void wcfGetUser()
+        {
+
+            Console.WriteLine("Starting getUserTest");
+            User user = proxy.createUser();
+            Console.WriteLine("User: " + user.ToString());
+            Assert.IsTrue(user.validate());
+        }
+    }
+}
---
+++ WorkoutMVC/Models/WCF/WCFClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibMyWorkout.Domain;
+using WorkoutMVC.ServiceReference1;
+
+namespace WorkoutMVC.Models.WCF
+{
+    public class WCFClient
+    {
+
+
+        public User createUser()
+        {
+            User user = null;
+
+            ServiceReference1.Service1Client proxy = new Service1Client();
+            user = proxy.createUser();
+
+            return user;
+        }
+
+
+
+
+
+    }
+}
